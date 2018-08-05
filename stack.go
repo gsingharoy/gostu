1c62845a93f62a2f709d406eb9a2882a8da9daa0
gsingharoy <neogauravsvnit@gmail.com>
2018-08-05 19:35:21 +0200

    All stack functionalities
    Added functionalities for Pop, Push, IsEmpty for the Stack

--- stack.go
+++ stack.go
@@ -1,5 +1,8 @@
 package gostu
 
+// Simple Stack data structure
+// This is a unidirectional simple data structure
+// read here to know more about stacks : https://www.geeksforgeeks.org/stack-data-structure/
 type Stack struct {
 	top *stackField
 }
@@ -12,4 +15,37 @@ type stackField struct {
 // NewStack() returns a pointer to a newly created empty Stack instance
 func NewStack() *Stack {
 	return &Stack{}
+}
+
+// Push(v interface{}) returns an error value
+// This method 'pushed' a value to the stack
+func (s *Stack) Push(v interface{}) error {
+	newField := &stackField{value: v, next: s.top}
+	s.top = newField
+	return nil
+}
+
+// Pop() returns the top value and an error if it exists
+// This method then sets the top to the next value
+func (s *Stack) Pop() (interface{}, error) {
+	if s.IsEmpty() {
+		return nil, ErrEmptyStack
+	}
+	v := s.top.value
+	s.top = s.top.next
+	return v, nil
+}
+
+// IsEmpty() returns true if the stack is empty
+func (s *Stack) IsEmpty() bool {
+	return s.top == nil
+}
+
+// Top() returns the top value of the Stack
+// Returns an error if the top is empty
+func (s *Stack) Top() (interface{}, error) {
+	if s.IsEmpty() {
+		return nil, ErrEmptyStack
+	}
+	return s.top.value, nil
 }
